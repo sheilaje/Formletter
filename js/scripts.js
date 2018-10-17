83672c66fe5a4e5ead8ab5b74c5577762b106997
leo mangutov and Sheila Stephen <sheila@>
2018-10-17 09:37:18 -0700

    add js file css file to the form letter

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#formOne form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    $(".person1").text(person1Input);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
